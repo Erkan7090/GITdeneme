a6de68a5ff448d7c5cd1869f493012d76f6c18e6
Erkan <sercan7090@gmail.com>
2020-05-07 18:36:05 +0200

    updated task java class void methid

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -2,10 +2,13 @@ package tests;
 
 public class task {
   
-  public static void main(String [] args){
-  
+  public static void main(String [] args) {
+
     System.out.println("Hello");
-    
   }
+
+  public void hi(){
+    System.out.println("from intelliJ");
+    }
 
 }
