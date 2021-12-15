2eaa7ade5080804993b73da13be41c6469d46446
Erkan <sercan7090@gmail.com>
2021-12-15 15:23:14 +0100

    ilk repo baslangic

---
+++ src/test/java/tests/logout.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class logout {
+
+    public static void main(String[] args) {
+
+        System.out.println("fd");
+        //this is something i added
+
+        System.out.println("hygit status");
+
+    }
+}
---
+++ src/test/java/tests/task.java
@@ -0,0 +1,22 @@
+package tests;
+
+public class task {
+  
+  public static void main(String [] args) {
+
+    System.out.println("Hello");
+  }
+
+
+  public void hi(){
+    System.out.println("from intelliJ");
+    }
+
+
+public void hello(){
+System.out.println("Hi from github");
+System.out.println("Hi 2222 from github");
+}
+             
+
+}
